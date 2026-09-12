30ffc5f4870a2e74c94ea6e7932c8949f51007d7
saritakoigade00-creator <saritakoigade00@gmail.com>
2026-09-12 16:49:49 +0530

    Update vite.config.js

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Arpita-task3/',
   plugins: [react(), tailwindcss()],
 })
